7f6ee6db730123e25e953e1367e2a4341a440fd1
hussienalbared <hussienalbared@gmail.com>
2018-03-29 14:44:51 +0200

    add search account component and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
-
+import { AccountSearchComponent } from './account-search/account-search.component';
+import { SearchAccountService } from './search-account.service';
+import { HttpModule , Http } from '@angular/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SearchAccountService, Http,HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/search-account.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SearchAccountService {
+
+  constructor() { }
+  showTodayDate() {
+    let ndate = 'hussien first service';
+    return ndate;
+ }
+}
