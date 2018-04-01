7af7bdeace3133355e5140208d2688661ac3c57a
hussienalbared <hussienalbared@gmail.com>
2018-04-01 17:10:17 +0200

    add bootstrap4 add search party component

---
+++ src/app/party-search/party-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-search',
+  templateUrl: './party-search.component.html',
+  styleUrls: ['./party-search.component.css']
+})
+export class PartySearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
