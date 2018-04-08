0b9ab4da8e750908a6cf7c5e0ffc41d242d56b94
hussienalbared <hussienalbared@gmail.com>
2018-04-08 15:41:04 +0200

    design table in material.io instaed of bootstrap4 add paginate to party search add sort to party search add filter to party search

--- src/app/main-content/main-content.component.spec.ts
+++ src/app/main-content/main-content.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SideMenuComponent } from './side-menu.component';
-import {MatMenuModule} from '@angular/material/menu';
-describe('SideMenuComponent', () => {
-  let component: SideMenuComponent;
-  let fixture: ComponentFixture<SideMenuComponent>;
+import { MainContentComponent } from './main-content.component';
+
+describe('MainContentComponent', () => {
+  let component: MainContentComponent;
+  let fixture: ComponentFixture<MainContentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SideMenuComponent ]
+      declarations: [ MainContentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SideMenuComponent);
+    fixture = TestBed.createComponent(MainContentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/party-search/party-search.component.ts
+++ src/app/party-search/party-search.component.ts
@@ -1,34 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { Http , Response} from '@angular/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
+import { MatPaginator, MatTableDataSource ,MatSort} from '@angular/material';
+
 import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Observable"
 @Component({
   selector: 'app-party-search',
   templateUrl: './party-search.component.html',
   styleUrls: ['./party-search.component.css']
 })
-export class PartySearchComponent implements OnInit {
-  PartyNumber:string='';
-  PartyId:string='';
-  PartyName:string='';
-  PoliticallyExposedPerson:string='';
-  Risk:string='';
-  PartyStatusDescription:string='';
-  result:any;
-  riskValue={1:'low',2:'medium',3:'high'};
-  constructor(private http:HttpClient) { }
+export class PartySearchComponent {
+  PartyNumber: string = '';
+  PartyId: string = '';
+  PartyName: string = '';
+  PoliticallyExposedPerson: string = '';
+  Risk: string = '';
+  PartyStatusDescription: string = '';
+  result: Party[];
+  dataSource:any=null;
+  riskValue = { 1: 'low', 2: 'medium', 3: 'high' };
+  displayedColumns = ['pno', 'pid', 'pfnm', 'ppepind', 'riskClassification', 'pstatds'];
+ 
+   @ViewChild(MatPaginator) paginator: MatPaginator;
+   @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
+
+
+  constructor(private http: HttpClient) { }
+
+  ngAfterViewInit() {
+   
   }
-  onClickSubmit(data){
-let url="http://localhost:8081/aml/api/party/search?PartyNumber="+this.PartyNumber+"&PartyId="+this.PartyId+
-"&PartyName="+this.PartyName+
-"&PoliticallyExposedPerson="+this.PoliticallyExposedPerson+
-"&Risk="+this.Risk+"&PartyStatusDescription="+this.PartyStatusDescription;
-this.http.get(url).subscribe(data => {
-  this.result=data;
-
+ 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  onClickSubmit(data) {
+    let url = "http://localhost:8081/aml/api/party/search?PartyNumber=" + this.PartyNumber + "&PartyId=" + this.PartyId +
+      "&PartyName=" + this.PartyName +
+      "&PoliticallyExposedPerson=" + this.PoliticallyExposedPerson +
+      "&Risk=" + this.Risk + "&PartyStatusDescription=" + this.PartyStatusDescription;
+    this.http.get<Party[]>(url).subscribe(data => {
+      this.result = data;
+     this.dataSource = new MatTableDataSource(data);
+     this.dataSource.paginator = this.paginator;
+     this.dataSource.sort = this.sort;
     });
+
+    console.log(this.result);
+  
   }
+
+}
+export interface Party {
+  pno: string;
+  pid: string;
+  pfnm: string;
+  ppepind: string;
+  riskClassification: string;
+  pstatds: string;
+
 
 }
