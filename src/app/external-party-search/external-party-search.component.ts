0a9d75a6f5cf9211bc6a2b7d7791d7b0763f1471
hussienalbared <hussienalbared@gmail.com>
2018-04-10 12:50:01 +0200

    add externalParty Interface add material table to externalParty Search add filter to externalParty Search add sort to externalParty Search add pagination to externalParty Search

--- src/app/external-party-search/external-party-search.component.ts
+++ src/app/external-party-search/external-party-search.component.ts
@@ -1,29 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { Http , Response} from '@angular/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
+
+import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Observable"
+import { externalParty } from '../models/externalParty.model';
 @Component({
   selector: 'app-external-party-search',
   templateUrl: './external-party-search.component.html',
   styleUrls: ['./external-party-search.component.css']
 })
 export class ExternalPartySearchComponent implements OnInit {
-result:any;
-ExtPartynumber:string="";
-ExtPartyId:string="";
-ExtPartyFullName:string="";
-ExtPartyIdCountryCode:string="";
-  constructor(private http:HttpClient) { }
+
+  ExtPartynumber: string = "";
+  ExtPartyId: string = "";
+  ExtPartyFullName: string = "";
+  ExtPartyIdCountryCode: string = "";
+  result: externalParty[];
+  dataSource: any = null;
+  displayedColumns = [
+    'expfullnm',
+    'expno',
+    'Address',
+    
+    'expstcinm',
+    'expbod',
+    'expstconm',
+    'expidtyds',
+    'expid',
+    // 'exptel1',
+    'TEL'
+  ];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
-  onClickSubmit(data){
-    let url="http://localhost:8081/aml/api/externalParty/all?ExtPartynumber="+this.ExtPartynumber+
-    "&ExtPartyId="+this.ExtPartyId+"&ExtPartyFullName="+this.ExtPartyFullName+
-    "&ExtPartyIdCountryCode="+this.ExtPartyIdCountryCode;
-    this.http.get(url).subscribe(data => {
-      this.result=data;
-    
-        });
-      }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  onClickSubmit(data) {
+    let url = "http://localhost:8081/aml/api/externalParty/all?ExtPartynumber=" + this.ExtPartynumber +
+      "&ExtPartyId=" + this.ExtPartyId + "&ExtPartyFullName=" + this.ExtPartyFullName +
+      "&ExtPartyIdCountryCode=" + this.ExtPartyIdCountryCode;
+    this.http.get<externalParty[]>(url).subscribe(data => {
+      this.result = data;
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+
+
+    });
+  }
 
 }
---
+++ src/app/models/externalParty.model.ts
@@ -0,0 +1,14 @@
+export interface externalParty{
+
+    expfullnm:string;
+    expno:string;
+    expstadd1:string;
+    expstadd2:string;
+    expstcinm:string;
+    expbod:string;
+    expstconm:string;
+    expidtyds:string;
+    expid:string;
+    exptel1:string;
+    exptel2:string;
+}
