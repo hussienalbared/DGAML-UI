3961926263cbc917deb9dec4c2a1ea70eaebabe2
DATAGEARBI\Mohamed.Farag <mohamed.farag@datagearbi.com>
2018-04-26 15:23:33 +0300

    under testing

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 @Injectable()
 export class AuthService {
 
 
   constructor(private http: HttpClient) { }
   login(credentials) {
-    console.log(credentials);
     console.log(JSON.stringify(credentials));
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json'
-      })
-    };
+
     return this.http.post('http://localhost:8081/aml/auth',
       JSON.stringify(credentials), httpOptions)
       .map(response => {
