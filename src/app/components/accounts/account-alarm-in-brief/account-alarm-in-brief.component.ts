f54485fb4e1103461293111a4c1a5d9b191a5251
hussienalbared <hussienalbared@gmail.com>
2018-05-16 14:03:19 +0200

    add account search fix alarm in brief fix transaction in brief

---
+++ src/app/components/accounts/account-alarm-in-brief/account-alarm-in-brief.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog, MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { AccountsService } from '../../../services/accounts.service';
+
+@Component({
+  selector: 'app-account-alarm-in-brief',
+  templateUrl: './account-alarm-in-brief.component.html',
+  styleUrls: ['./account-alarm-in-brief.component.css']
+})
+export class AccountAlarmInBriefComponent implements OnInit {
+  AccountKey: string;
+  displayedColumns = ['select', 'alarmId', 'primaryObjLevelCode', 'routineName',
+  'routineCategoryCode', 'routineDescription', 'runDate'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  selection = new SelectionModel<any>(true, []);
+  dataSource: any = [];
+ 
+  constructor( private route: ActivatedRoute,
+    private router: Router,
+    public dialog: MatDialog,
+    private suspectService: AccountsService) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.AccountKey = params.get('account_Key');
+     
+      this.getAlarms();
+
+    });
+  }
+  getAlarms() {
+  
+   
+    this.suspectService.getAccountAlarmsInBrief(this.AccountKey).subscribe(data => {
+      this.dataSource = data;
+
+      this.dataSource = new MatTableDataSource(this.dataSource);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+   
+
+    });
+
+  }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+}
---
+++ src/app/components/accounts/section3/section3.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-section3',
+  templateUrl: './section3.component.html',
+  styleUrls: ['./section3.component.css']
+})
+export class Section3Component implements OnInit {
+
+  dataSource :any/*= ELEMENT_DATA*/;
+  result: TransactionBrief[];
+  displayedColumns = ['ACCTNO', 'AcctKey','TTRN', 'CFDATEKEY', 'CFCURRAMT','transaction_cdi_desc','RTTDS'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+   @ViewChild(MatSort) sort: MatSort;
+   applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  constructor(private http:HttpClient,
+    private route: ActivatedRoute,private router: Router
+  ) { }
+
+  ngOnInit() {
+
+    this.route.paramMap.subscribe(params => {
+      let accountNumber=params.get('accountNumber');
+   
+      this.getData(accountNumber);  
+    });
+  }
+  getData(number){
+    let Url="http://localhost:8081/aml/api/account/accountDetailSection3?accountNo="+number;
+    this.http.get<TransactionBrief[]>(Url).subscribe(data=>{
+
+      this.dataSource=data;
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      
+          });
+
+  }
+  
+
+}
+export interface TransactionBrief{
+  acct_Key:string, 
+  trans_Ref_No:string,
+  date_Key:string, 
+  ccy_Amt:string,
+  trans_Cr_Db_Ind_Desc:string,
+   trans_Desc:string
+
+}
