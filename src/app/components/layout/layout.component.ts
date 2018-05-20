c550b21986e4115a12f21bfe4b53c56d21b1c17f
Amr_Almajeedi <amabedu0@gmail.com>
2018-05-20 10:41:21 -0700

    add active style of side bar links

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatMenuModule } from '@angular/material/menu';
@@ -12,11 +13,30 @@ export class LayoutComponent implements OnInit {
   mode = new FormControl('side');
   isOn = true;
   elemen: any;
+  isActive = false;
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
-  }
+  } 
   changeIsOn() {
     this.isOn = !this.isOn;
-  }
-}
+  } 
+  /* --- */
+
+  layoutItem(e){
+    
+    for (var _i = 0; _i < document.getElementsByClassName("mat_item_a").length; _i++) {
+      document.getElementsByClassName("mat_item_a")[_i].classList.remove("active");
+      document.getElementsByClassName("mat-list-item-content")[_i].classList.remove("active");
+    }
+    $(e.target).addClass('active');
+    //$(e.target).css({ "display": "block !important","background-color": "#F0843F !important"});
+    // this.isActive = true;
+    // // e.target.addClass("active")
+    //  $(e.target).addClass("active");
+    
+    //
+      console.log(" = " + $(e.target).attr("class"));
+    }
+  /* --- */
+}   
