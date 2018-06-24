901ebcc3b2be3b1c8202a73f780d54729cca901b
Amr_Almajeedi <amabedu0@gmail.com>
2018-06-24 10:06:24 -0700

    get select clicked row

--- src/app/components/user/update-user/update-user.component.ts
+++ src/app/components/user/update-user/update-user.component.ts
@@ -1,3 +1,4 @@
+import { user } from './../../../models/user.model';
 import { UserService } from './../../../services/user.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Component, OnInit, Inject } from '@angular/core';
@@ -10,7 +11,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 export class UpdateUserComponent implements OnInit {
 
   username: string;
-  DisplayName_: string;
+  DisplayName: string;
   password: string;
   firstname: string;
   lastname: string;
@@ -20,14 +21,32 @@ export class UpdateUserComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<UpdateUserComponent>,private userService: UserService
   ,@Inject(MAT_DIALOG_DATA) public data: any) { 
-      
+    console.log("in update constractor");
+    console.log(data);
+    // const user_: user = {  
+    //   username: this.username,
+    //   displayName: this.DisplayName,
+    //   firstname: this.firstname,
+    //   password: this.password,
+    //   lastname: this.lastname,
+    //   email: this.email,
+    //   enabled: this.enabled,
+    //   lastPasswordResetDate: this.lastPasswordResetDate
+    // }
+    this.username = data.username;
+    this.DisplayName = data.email;
+    this.enabled = data.enabled;
+    this.firstname = data.firstname;
+    this.lastPasswordResetDate = data.lastPasswordResetDate;
+    this.lastname = data.lastname;
+    this.password = data.password;   
   }
 
   ngOnInit() {
   }
 
   updateUser(){
-    this.userService.updateUser(this.username,this.DisplayName_,this.password,this.firstname,this.lastname,this.email);
+    this.userService.updateUser(this.username,this.DisplayName,this.password,this.firstname,this.lastname,this.email);
   }
 
 }
