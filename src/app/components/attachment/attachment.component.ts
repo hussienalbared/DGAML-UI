ae1a8208935bb64d048ebcec9847ece2d6d94d2f
hussienalbared <hussienalbared@gmail.com>
2018-07-03 17:42:38 +0200

    update style

--- src/app/components/attachment/attachment.component.ts
+++ src/app/components/attachment/attachment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AttachmentService } from '../../services/attachment.service';
 import {saveAs } from "file-saver";
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-attachment',
   templateUrl: './attachment.component.html',
@@ -9,13 +10,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AttachmentComponent implements OnInit {
   f: FileList;
-  // @ViewChild('ff') el: ElementRef;
   @ViewChild('files') el2: ElementRef;
   attachments:any=null;
   description:string=''
   key:string
   code:string
-  constructor(private attachmentService: AttachmentService,private route:ActivatedRoute) { }
+  constructor(private attachmentService: AttachmentService,private route:ActivatedRoute,private userService:UserService) { }
   
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -33,9 +33,14 @@ export class AttachmentComponent implements OnInit {
   }
 
   uploadAll() {
+    
     let userId=+localStorage.getItem('id')
-   this.attachmentService.uploadFiles(this.el2.nativeElement.files,this.key,this.code,this.description,userId).subscribe();
-    // this.attachmentService.uploadFiles(this.el.nativeElement.files,this.key,this.code,this.description,userId).subscribe();
+   this.attachmentService.uploadFiles(this.el2.nativeElement.files,this.key,this.code,this.description,userId).subscribe(
+data=>{
+  this.attachments=data;
+}
+
+   );
 
 
   }
@@ -46,6 +51,7 @@ export class AttachmentComponent implements OnInit {
 
   }
   deleteAttachment(id){
+    console.log(id)
 if(confirm("Are you sure you want to delete?"))
 {
 this.attachmentService.deleteAttachment(id).subscribe(
