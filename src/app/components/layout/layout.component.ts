c29934ad306f0ad5e01cee80066577275139697d
hussienalbared <hussienalbared@gmail.com>
2018-07-18 15:40:27 +0200

    add userSpecific notification

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -63,7 +63,7 @@ if(this.newValue!==this.notificatiobValue){
   });
 }); 
 
-  this.notification.allNoti().subscribe(data=>{
+  this.notification.allNoti2().subscribe(data=>{
 
 
     this.notifications = data;
