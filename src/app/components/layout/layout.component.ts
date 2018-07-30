48d697f9dd38cacbe73db3dea3fa9ccfd613562c
Mohammed Farag <mohamed.farag@datagearbi.com>
2018-07-30 15:27:12 +0200

    review code with tslint

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -20,11 +20,11 @@ import { environment } from '../../../environments/environment'; @Component({
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-  activeUrl: string = '';
+  activeUrl = '';
   mode = new FormControl('side');
   isOn = true;
   elemen: any;
-  userName: string = '';
+  userName = '';
   notificatiobValue: Number;
   newValue: number;
   notifications: any = [];
@@ -44,7 +44,7 @@ export class LayoutComponent implements OnInit {
     // $(document).ready(()=>{
     //   $(window).click(function(e) {
     //     alert("A")
-    //     $('.noti-block').hide();     
+    //     $('.noti-block').hide();
     //   })
     // });
 
@@ -60,26 +60,21 @@ export class LayoutComponent implements OnInit {
     });
 
     if (localStorage.getItem('name') !== null) {
-      this.userName = localStorage.getItem('name')
+      this.userName = localStorage.getItem('name');
     }
 
     /******** */
-    let stompClient = this.webSocketService.connect();
+    const stompClient = this.webSocketService.connect();
     stompClient.connect({}, frame => {
 
       stompClient.subscribe(`/topic/notification/${localStorage.getItem('id')}`, noti => {
-        ;
-
-      
-    
-
 
         this.notifications.push(JSON.parse(noti.body));
 
         // this.notifications = JSON.parse(noti.body);
         this.newValue = this.notifications.length;
         if (this.newValue !== this.notificatiobValue) {
-          this.ffff()
+          this.ffff();
         }
         this.notificatiobValue = this.notifications.length;
 
@@ -93,33 +88,34 @@ export class LayoutComponent implements OnInit {
 
 
       this.notifications = data;
-     
+
       this.notificatiobValue = this.notifications.length;
     });
   }
 
   changeIsOn() {
     this.isOn = !this.isOn;
-    if (this.isOn)
-      $(".topbar[_ngcontent-c1] .topbar-left[_ngcontent-c1]").css({ 'padding-right': '10px', 'padding-left': '0px' });
-    else
-      $(".topbar[_ngcontent-c1] .topbar-left[_ngcontent-c1]").css('padding-left', '6px');
+    if (this.isOn) {
+      $('.topbar[_ngcontent-c1] .topbar-left[_ngcontent-c1]').css({ 'padding-right': '10px', 'padding-left': '0px' });
+    } else {
+      $('.topbar[_ngcontent-c1] .topbar-left[_ngcontent-c1]').css('padding-left', '6px');
+    }
   }
   /* --- */
 
   layoutItem(e) {
 
-    for (var _i = 0; _i < $(".mat_item_a").length; _i++) {
-      $(".mat_item_a")[_i].classList.remove("active");
-      $(".mat-list-item-content")[_i].classList.remove("active");
+    for (let _i = 0; _i < $('.mat_item_a').length; _i++) {
+      $('.mat_item_a')[_i].classList.remove('active');
+      $('.mat-list-item-content')[_i].classList.remove('active');
     }
     $(e.target).addClass('active');
     $(e.target).closest('.mat_item_a').addClass('active');
-    
+
   }
   navItem(url) {
-    this.router.navigate(["/" + url]);
-    let y = "/" + url;
+    this.router.navigate(['/' + url]);
+    const y = '/' + url;
 
 
 
@@ -131,7 +127,7 @@ export class LayoutComponent implements OnInit {
 
   }
   removeTab(path, label) {
-    let Tab: tab = { path: path, label: label }
+    const Tab: tab = { path: path, label: label };
     this.TabService.removeTab(Tab);
   }
 
@@ -143,55 +139,54 @@ export class LayoutComponent implements OnInit {
 
 
 /*****ASZ******/
-changeLang2(tarnsLang:string){
+changeLang2(tarnsLang: string) {
   // alert(tarnsLang)
   this.translate.setDefaultLang(tarnsLang);
-  this.translate.use(tarnsLang)
+  this.translate.use(tarnsLang);
   this.selected_Language = tarnsLang;
-  
 
-  //--- temp because it doesn't read the style from ar/en.json
-    if(this.selected_Language=='en'){
+
+  // --- temp because it doesn't read the style from ar/en.json
+    if (this.selected_Language === 'en') {
       $('.all_container').css('text-align', 'left' );
       $('.select_button').css('text-align', 'left' );
-    }
-    else {
+    } else {
       $('.all_container').css('text-align', 'right');
       $('.select_button').css('text-align', 'right');
     }
-    //---
+    // ---
   }
   /***********/
 
   updateProfile() {
-    this.router.navigate(["profile"])
+    this.router.navigate(['profile']);
   }
   goTodashboard() {
-    this.router.navigate(["/empty"])
-    window.open("http://192.168.1.40/Reports/Pages/Report.aspx?ItemPath=%2fAML+Project%2fDASHBOARD%2fAML+Main+Dashboard", "_blank");
+    this.router.navigate(['/empty']);
+    window.open('http://192.168.1.40/Reports/Pages/Report.aspx?ItemPath=%2fAML+Project%2fDASHBOARD%2fAML+Main+Dashboard', '_blank');
   }
   stopNotificationAnimation() {
     $(document).ready(() => {
 
 
-      $("#Notification1").removeClass("notify")
+      $('#Notification1').removeClass('notify');
 
-    })
+    });
   }
   ffff() {
     $(document).ready(() => {
 
 
-      $("#Notification1").addClass("notify")
+      $('#Notification1').addClass('notify');
 
-    })
+    });
   }
 
   /////////////
   sendNoti(x, index: number) {
 
 
-    this.notification.sendNoti(x.notification_ID, localStorage.getItem('id'))
+    this.notification.sendNoti(x.notification_ID, localStorage.getItem('id'));
 
     this.notifications.splice(this.notifications.length - index - 1, 1);
     this.notificatiobValue = this.notifications.length - 1;
@@ -200,8 +195,8 @@ changeLang2(tarnsLang:string){
 
 
   markAllRead() {
-    console.log("markAllRead()-markAllRead() Function")
-    this.notification.markAllRead(localStorage.getItem('id'))
+    console.log('markAllRead()-markAllRead() Function');
+    this.notification.markAllRead(localStorage.getItem('id'));
     this.notifications = [];
     this.notificatiobValue = 0;
   }
