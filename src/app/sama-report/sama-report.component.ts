7cc5cb17d7ba11bc8e88d1e4af84f7ec09abe971
hussienalbared <hussienalbared@gmail.com>
2018-08-01 17:49:03 +0200

    fix service url

--- src/app/sama-report/sama-report.component.ts
+++ src/app/sama-report/sama-report.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Options } from 'selenium-webdriver/edge';
+import { environment } from '../../environments/environment';
 
 
 
@@ -27,9 +28,9 @@ export class SamaReportComponent  {
    }
 
   loadTransactionList(customerId: HTMLInputElement){
-    let url="http://localhost:8081/aml/api/suspectedTransaction/all?partyNumber="+customerId.value;
-    // this.http.get('http://localhost:8081/report/getTransactions?customerId='+customerId.value)
-    this.http.get(url)
+    this.http.get(environment.ipAddress+'/report/getTransactions?customerId='+customerId.value)
+    // let url=environment.ipAddress+'/aml/api/suspectedTransaction/all?partyNumber='+customerId.value;
+    // this.http.get(url)
     .subscribe( response => {
       this.transactionList = JSON.parse(JSON.stringify(response));
       console.log(this.transactionList)
@@ -39,9 +40,14 @@ export class SamaReportComponent  {
 
   generateSamaReport(){
    this.transactionList = null;
-   let url = 'http://localhost:8081/report/createSamaReport';
+   let url = environment.ipAddress+'/report/createSamaReport';
+   console.log('selected transaction');
+   console.log(this.selectedTransaction);
    this.http.post(url , this.selectedTransaction).subscribe(response =>{
      this.samaReports = JSON.parse(JSON.stringify(response));
+     console.log('WWWWWWWWWWW');
+     console.log(this.samaReports);
+     console.log('WWWWWWWWWWWWWww');
     });
   }
 
@@ -50,13 +56,13 @@ export class SamaReportComponent  {
     console.log('HHHHHH')
     console.log(SamaReport)
     let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    let url = 'http://localhost:8081/report/saveSamaReport';
+    let url = environment.ipAddress+'/report/saveSamaReport';
     this.http.post(url, SamaReport, {headers: headers}).subscribe(response =>{
     });
   }
 
   downloadReport(lang : String){
-    let url = 'http://localhost:8081/report/printSamaReport?transactionIds='+this.selectedTransaction+'&lang='+lang;
+    let url = environment.ipAddress+'/report/printSamaReport?transactionIds='+this.selectedTransaction+'&lang='+lang;
     let headers = new HttpHeaders();
     headers = headers.set('Accept', 'application/pdf');
      this.http.get(url, { headers: headers, responseType: 'blob' }).subscribe(response =>{
